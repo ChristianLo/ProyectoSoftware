356cd8045a7c3446c612d9c771dbd49ba4d92bec
christian <christian.lopeza@mail.udp.cl>
2017-11-18 21:43:40 -0300

    funcion get_email()

--- app/setup.py
+++ app/setup.py
@@ -1,6 +1,7 @@
-from config import *
 import psycopg2
-conn = psycopg2.connect("dbname=%s host=%s user=%s password=%s"%(database,host,user,password))
+from config import *
+
+conn = psycopg2.connect("dbname=%s host=%s user=%s password=%s" % (database, host, user, password))
 
 cur = conn.cursor()
 
@@ -11,19 +12,34 @@ cur = conn.cursor()
 # cur.execute(sql)
 
 sql = """
-CREATE TABLE sucursal( id serial PRIMARY KEY , id_adm_local integer , calle varchar, nombre varchar , comuna varchar, ciudad varchar, region varchar, telefono integer);
-CREATE TABLE usuario(id serial PRIMARY KEY, tipo varchar, nombre varchar, rut integer, password varchar, telefono integer , email varchar );
-CREATE TABLE productos(id serial PRIMARY KEY , nombre varchar , cantidad integer, detalle varchar);
-CREATE TABLE ventas(num_venta serial PRIMARY KEY , tipo integer, cliente_id integer, fechahora timestamp);
-CREATE TABLE ventas_detalle(num_venta integer, tipo_compra integer, id_compra integer, monto integer , cantidad integer);
-CREATE TABLE cupon(id serial PRIMARY KEY, precio integer, fecha_inicio timestamp, fecha_termino timestamp);
-CREATE TABLE cupon_detalle(cupon_id integer, producto_id integer, cantidad integer);
-CREATE TABLE promocion(id serial PRIMARY KEY, precio integer);
-CREATE TABLE promocion_detalle(promo_id integer, producto_id integer, cantidad integer);
+CREATE TABLE sucursal( id SERIAL PRIMARY KEY , id_adm_local INTEGER , calle VARCHAR, nombre VARCHAR , comuna VARCHAR, ciudad VARCHAR, region VARCHAR, telefono INTEGER);
+CREATE TABLE usuario(id SERIAL PRIMARY KEY, tipo VARCHAR, nombre VARCHAR, rut INTEGER, password VARCHAR, telefono INTEGER , email VARCHAR );
+CREATE TABLE productos(id SERIAL PRIMARY KEY , nombre VARCHAR , cantidad INTEGER, detalle VARCHAR);
+CREATE TABLE ventas(num_venta SERIAL PRIMARY KEY , tipo INTEGER, cliente_id INTEGER, fechahora TIMESTAMP);
+CREATE TABLE ventas_detalle(num_venta INTEGER, tipo_compra INTEGER, id_compra INTEGER, monto INTEGER , cantidad INTEGER);
+CREATE TABLE cupon(id SERIAL PRIMARY KEY, precio INTEGER, fecha_inicio TIMESTAMP, fecha_termino TIMESTAMP);
+CREATE TABLE cupon_detalle(cupon_id INTEGER, producto_id INTEGER, cantidad INTEGER);
+CREATE TABLE promocion(id SERIAL PRIMARY KEY, precio INTEGER);
+CREATE TABLE promocion_detalle(promo_id INTEGER, producto_id INTEGER, cantidad INTEGER);
 """
 
-#queda con 255 el varchar
-cur.execute(sql)
+
+def get_email(id):
+    try:
+        if type(id) is int:
+            id = str(id)
+        cur.execute("""SELECT email FROM usuario WHERE id = %s""", id)
+        email = cur.fetchall()
+        if not email:
+            return None, 'info'
+        return email[0][0], 'success'
+    except psycopg2.Error as e:
+        print(e)
+
+
+print(get_email(1))
+# queda con 255 el varchar
+# cur.execute(sql)
 conn.commit()
 cur.close()
 conn.close()
